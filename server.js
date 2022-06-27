b3e32b7c28e871820c8bd9fb9a8416bf9c586dc0
christian <christiancarvajal@Christians-MacBook-Air.local>
2022-06-27 15:42:57 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const inquirer =require('inquirer');
+
+const questions = [
+    {
+        type: 'list',
+        name: 'options',
+        message: 'what would you like to do?',
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 
+        'add a role', 'add an employee', 'update an employee role']
+    } , 
+    {
+        type: 'list',
+        name: 'departments',
+        message: 'view all departments',
+        choices: ['1', '2', '3'],
+        when: (answers) => answers.options === 'view all departments'
+    },
+    {
+        type: 'list',
+        name: 'roles',
+        message: 'view all roles',
+        choices: ['1', '2', '3'],
+        when: (answers) => answers.options === 'view all roles'
+
+    }
+]
+
+function init() {
+    inquirer.prompt(questions);
+}
+init();
